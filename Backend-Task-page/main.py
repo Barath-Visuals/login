0ee28b12708cbc6c4ebe68ccb53c0539d00d0255
Barath-Visuals <barathbm2003@gmail.com>
2025-10-05 14:37:51 +0530

    added chamges

--- Backend-Task-page/main.py
+++ Backend-Task-page/main.py
@@ -59,5 +59,5 @@ def robots():
 # Start server (Railway dynamic port)
 # ------------------
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 8000))
     uvicorn.run("main:app", host="0.0.0.0", port=port)
